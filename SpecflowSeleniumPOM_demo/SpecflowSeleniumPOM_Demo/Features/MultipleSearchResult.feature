5316a1f2d322a92a15c787a0ea6ec4c6d714348c
Sanam Maharjan <newamacha007@gmail.com>
2024-06-05 18:10:34 +0545

    additional proj

---
+++ SpecflowSeleniumPOM_demo/SpecflowSeleniumPOM_Demo/Features/MultipleSearchResult.feature
@@ -0,0 +1,21 @@
+﻿Feature: MultipleSearchResult
+
+@regression @search
+Scenario: Multiple Search Books in Bookstore
+	Given I navigate to Bookstore application
+	And I click on Book Store Application
+	When I fill up the search box
+		| Keyword    |
+		| JavaScript |
+	Then following books should be displayed in results
+		| Title                               | Author           | Publisher       |
+		| Learning JavaScript Design Patterns | Addy Osmani      | O'Reilly Media  |
+		| Speaking JavaScript                 | Axel Rauschmayer | O'Reilly Media  |
+		| Programming JavaScript Applications | Eric Elliott     | O'Reilly Media  |
+		| Eloquent JavaScript, Second Edition | Marijn Haverbeke | No Starch Press |
+
+#Examples:
+#	| Keyword | Title                                     | Author               | Publisher      |
+#	| GIT     | Git Pocket Guide                          | Richard E. Silverman | O'Reilly Media |
+#	| Web Api | Designing Evolvable Web APIs with ASP.NET | Glenn Block et al.   | O'Reilly Media |
+#	| Learn   | Learning JavaScript Design Patterns       | Addy Osmani          | O'Reilly Media |
---
+++ SpecflowSeleniumPOM_demo/SpecflowSeleniumPOM_Demo/StepDefinitions/BooksSteps.cs
@@ -0,0 +1,58 @@
+﻿using AngleSharp.Common;
+using OpenQA.Selenium.Support.Extensions;
+using SpecFlowPSpecflowSeleniumPOM_Demo.Support;
+using SpecflowSeleniumPOM_Demo.Config;
+using SpecflowSeleniumPOM_Demo.Pages;
+using SpecflowSeleniumPOM_Demo.Support;
+using SpecFlSpecflowSeleniumPOM_DemowProject.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecflowSeleniumPOM_Demo.StepDefinitions
+{
+    [Binding]
+    internal class BooksSteps
+    {
+        private WebDriverSupport _driverSupport;
+        BooksPage booksPage;
+        JSExecutor jSExecutor;
+
+        public BooksSteps(WebDriverSupport driverSupport)
+        {
+            _driverSupport = driverSupport;
+            booksPage = new BooksPage(_driverSupport.Driver);
+            jSExecutor = new JSExecutor(_driverSupport.Driver);
+        }
+
+        [Given(@"I click on Book Store Application")]
+        public void GivenIClickOnBookStoreApplication()
+        {
+            jSExecutor.ScrollIntoElement(booksPage.BookStoreApp);
+            booksPage.BookStoreApp.Click();
+        }
+
+        [When(@"I fill up the search box")]
+        public void WhenIFillUpTheSearchBox(Table table)
+        {
+            var rows = table.Rows;
+            booksPage.SearchBoxInput.SendKeys(rows[0][0]);
+            Thread.Sleep(3000);
+        }
+
+        [Then(@"following books should be displayed in results")]
+        public void ThenFollowingBooksShouldBeDisplayedInResults(Table table)
+        {
+            var Rows = table.CreateSet<ResultVariables>();
+            //var row = Rows.ElementAt(0);
+            foreach(var row in Rows)
+            {
+                booksPage.SearchResult(row.Title, row.Author, row.Publisher);
+            }
+        }
+
+    }
+}
